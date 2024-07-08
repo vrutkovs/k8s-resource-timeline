1a406ddfb5b3fb7ffa1f59ffa6d66cd90a52a6a2
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-07-08 08:47:32 +0200

    Watch pods, configmaps, secrets and nodes

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use futures::TryStreamExt;
 use futures_core::stream::Stream;
 use futures_util::pin_mut;
 use futures_util::stream::StreamExt;
+use merge_streams::MergeStreams;
 
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::api::core::v1::{ConfigMap, Node, Pod, Secret};
 use kube::{
     api::Api,
     runtime::{predicates, reflector, watcher, WatchStreamExt},
@@ -22,12 +23,15 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
 
-    let pod_stream = watch_pods(client).await;
-    pin_mut!(pod_stream);
+    let pod_stream = watch_pods(client.clone()).await;
+    let cm_stream = watch_configmaps(client.clone()).await;
+    let secret_stream = watch_secrets(client.clone()).await;
+    let node_stream = watch_nodes(client.clone()).await;
+    let mut s = (pod_stream, cm_stream, secret_stream, node_stream).merge();
 
-    while let Some(maybe_value) = pod_stream.next().await {
+    while let Some(maybe_value) = s.next().await {
         if let Ok(value) = maybe_value {
-            println!("got {}", value);
+            println!("{}", value);
         }
     }
 
@@ -35,30 +39,24 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn watch_pods(client: Client) -> impl Stream<Item = Result<String, anyhow::Error>> {
-    let mut pod_table: HashMap<String, String> = HashMap::new();
+    let mut cache: HashMap<String, String> = HashMap::new();
     stream! {
-        let pod_api: Api<Pod> = Api::all(client);
-        let (_, pod_writer) = reflector::store::<Pod>();
-        let pod_stream = watcher(pod_api, watcher::Config::default().any_semantic())
+        let client_api: Api<Pod> = Api::all(client);
+        let (_, writer) = reflector::store::<Pod>();
+        let stream = watcher(client_api, watcher::Config::default().any_semantic())
             .default_backoff()
-            .reflect(pod_writer)
+            .reflect(writer)
             .applied_objects()
             .predicate_filter(predicates::resource_version);
-        pin_mut!(pod_stream);
+        pin_mut!(stream);
 
-        while let Some(pod) = pod_stream.try_next().await? {
+        while let Some(pod) = stream.try_next().await? {
             let resource_name = format!(
                 "namespace {} pod {}",
                 pod.namespace().unwrap_or("".into()),
                 pod.name_any()
             );
-            info!(
-                "{}: rv {}",
-                resource_name,
-                pod.resource_version().unwrap_or("".into())
-            );
-
-            let previous_yaml: String = pod_table.get(&resource_name).unwrap_or(&"".into()).into();
+            let previous_yaml: String = cache.get(&resource_name).unwrap_or(&"".into()).into();
             let yaml = serde_yaml::to_string(&pod).unwrap();
             let input = InternedInput::new(previous_yaml.as_str(), yaml.as_str());
             let diff = diff(
@@ -66,7 +64,102 @@ async fn watch_pods(client: Client) -> impl Stream<Item = Result<String, anyhow:
                 &input,
                 UnifiedDiffBuilder::new(&input),
             );
-            pod_table.entry(resource_name).or_insert(yaml);
+            cache.entry(resource_name).or_insert(yaml);
+            yield Ok(diff);
+        }
+    }
+}
+
+async fn watch_configmaps(client: Client) -> impl Stream<Item = Result<String, anyhow::Error>> {
+    let mut cache: HashMap<String, String> = HashMap::new();
+    stream! {
+        let client_api: Api<ConfigMap> = Api::all(client);
+        let (_, writer) = reflector::store::<ConfigMap>();
+        let stream = watcher(client_api, watcher::Config::default().any_semantic())
+            .default_backoff()
+            .reflect(writer)
+            .applied_objects()
+            .predicate_filter(predicates::resource_version);
+        pin_mut!(stream);
+
+        while let Some(configmap) = stream.try_next().await? {
+            let resource_name = format!(
+                "namespace {} configmap {}",
+                configmap.namespace().unwrap_or("".into()),
+                configmap.name_any()
+            );
+            let previous_yaml: String = cache.get(&resource_name).unwrap_or(&"".into()).into();
+            let yaml = serde_yaml::to_string(&configmap).unwrap();
+            let input = InternedInput::new(previous_yaml.as_str(), yaml.as_str());
+            let diff = diff(
+                Algorithm::Histogram,
+                &input,
+                UnifiedDiffBuilder::new(&input),
+            );
+            cache.entry(resource_name).or_insert(yaml);
+            yield Ok(diff);
+        }
+    }
+}
+
+async fn watch_secrets(client: Client) -> impl Stream<Item = Result<String, anyhow::Error>> {
+    let mut cache: HashMap<String, String> = HashMap::new();
+    stream! {
+        let client_api: Api<Secret> = Api::all(client);
+        let (_, writer) = reflector::store::<Secret>();
+        let stream = watcher(client_api, watcher::Config::default().any_semantic())
+            .default_backoff()
+            .reflect(writer)
+            .applied_objects()
+            .predicate_filter(predicates::resource_version);
+        pin_mut!(stream);
+
+        while let Some(secret) = stream.try_next().await? {
+            let resource_name = format!(
+                "namespace {} secret {}",
+                secret.namespace().unwrap_or("".into()),
+                secret.name_any()
+            );
+            let previous_yaml: String = cache.get(&resource_name).unwrap_or(&"".into()).into();
+            let yaml = serde_yaml::to_string(&secret).unwrap();
+            let input = InternedInput::new(previous_yaml.as_str(), yaml.as_str());
+            let diff = diff(
+                Algorithm::Histogram,
+                &input,
+                UnifiedDiffBuilder::new(&input),
+            );
+            cache.entry(resource_name).or_insert(yaml);
+            yield Ok(diff);
+        }
+    }
+}
+
+async fn watch_nodes(client: Client) -> impl Stream<Item = Result<String, anyhow::Error>> {
+    let mut cache: HashMap<String, String> = HashMap::new();
+    stream! {
+        let client_api: Api<Node> = Api::all(client);
+        let (_, writer) = reflector::store::<Node>();
+        let stream = watcher(client_api, watcher::Config::default().any_semantic())
+            .default_backoff()
+            .reflect(writer)
+            .applied_objects()
+            .predicate_filter(predicates::resource_version);
+        pin_mut!(stream);
+
+        while let Some(node) = stream.try_next().await? {
+            let resource_name = format!(
+                "node {}",
+                node.name_any()
+            );
+            let previous_yaml: String = cache.get(&resource_name).unwrap_or(&"".into()).into();
+            let yaml = serde_yaml::to_string(&node).unwrap();
+            let input = InternedInput::new(previous_yaml.as_str(), yaml.as_str());
+            let diff = diff(
+                Algorithm::Histogram,
+                &input,
+                UnifiedDiffBuilder::new(&input),
+            );
+            cache.entry(resource_name).or_insert(yaml);
             yield Ok(diff);
         }
     }
