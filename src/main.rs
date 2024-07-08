d7f178fc96235d40b1a073c0b6c51ee3abe7eb16
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-07-08 12:05:21 +0200

    Use generics, store change timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use futures_util::stream::StreamExt;
 use merge_streams::MergeStreams;
 
+use k8s_openapi::api::core::v1::{ConfigMap, Node, Pod, Secret};
 use kube::Client;
 
 mod watcher;
@@ -10,10 +11,10 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
 
-    let pod_stream = watcher::watch_pods(client.clone()).await;
-    let cm_stream = watcher::watch_configmaps(client.clone()).await;
-    let secret_stream = watcher::watch_secrets(client.clone()).await;
-    let node_stream = watcher::watch_nodes(client.clone()).await;
+    let pod_stream = watcher::watch_resource::<Pod>(client.clone()).await;
+    let cm_stream = watcher::watch_resource::<ConfigMap>(client.clone()).await;
+    let secret_stream = watcher::watch_resource::<Secret>(client.clone()).await;
+    let node_stream = watcher::watch_resource::<Node>(client.clone()).await;
     let mut s = (pod_stream, cm_stream, secret_stream, node_stream).merge();
 
     while let Some(maybe_value) = s.next().await {
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use std::collections::HashMap;
 
 use async_stream::stream;
@@ -16,7 +17,7 @@ use imara_diff::intern::InternedInput;
 use imara_diff::{diff, Algorithm, UnifiedDiffBuilder};
 
 #[derive(Debug)]
-enum ChangeType {
+pub enum ChangeType {
     Metadata,
     Spec,
     Status,
@@ -26,35 +27,60 @@ pub struct ObjectDiff {
     resource_name: String,
     diff: String,
     change: ChangeType,
+    timestamp: DateTime<Utc>,
 }
 
 impl std::fmt::Display for ObjectDiff {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}: change {:?} diff\n{}",
-            self.resource_name, self.change, self.diff
+            "{}: resource {} change {:?} diff\n{}",
+            self.timestamp, self.resource_name, self.change, self.diff
         )
     }
 }
 
-trait WatchDiff<T> {
+pub trait ExtraInfo<T> {
     fn resource_name(&self) -> String;
-    fn yaml(&self) -> String;
     fn change_type(&self, previous: &T) -> ChangeType;
+}
+
+pub trait WatchDiff<T> {
+    fn yaml(&self) -> String;
     fn diff(&self, previous: &T) -> ObjectDiff;
 }
 
-impl WatchDiff<Pod> for Pod {
+impl<T> WatchDiff<T> for T
+where
+    T: kube::Resource + k8s_openapi::serde::Serialize + ExtraInfo<T>,
+{
+    fn yaml(&self) -> String {
+        serde_yaml::to_string(&self).unwrap()
+    }
+    fn diff(&self, previous: &T) -> ObjectDiff {
+        let src: String = previous.yaml();
+        let dst: String = self.yaml();
+        let input = InternedInput::new(src.as_str(), dst.as_str());
+        ObjectDiff {
+            timestamp: Utc::now(),
+            change: self.change_type(previous),
+            resource_name: self.resource_name(),
+            diff: diff(
+                Algorithm::Histogram,
+                &input,
+                UnifiedDiffBuilder::new(&input),
+            ),
+        }
+    }
+}
+
+impl ExtraInfo<Pod> for Pod {
     fn resource_name(&self) -> String {
         format!(
             "namespace {} pod {}",
             self.namespace().unwrap_or("".into()),
             self.name_any()
         )
-    }
-    fn yaml(&self) -> String {
-        serde_yaml::to_string(&self).unwrap()
     }
     fn change_type(&self, previous: &Pod) -> ChangeType {
         if self.spec != previous.spec {
@@ -65,23 +91,9 @@ impl WatchDiff<Pod> for Pod {
         }
         ChangeType::Metadata
     }
-    fn diff(&self, previous: &Pod) -> ObjectDiff {
-        let src: String = previous.yaml();
-        let dst: String = self.yaml();
-        let input = InternedInput::new(src.as_str(), dst.as_str());
-        ObjectDiff {
-            change: self.change_type(previous),
-            resource_name: self.resource_name(),
-            diff: diff(
-                Algorithm::Histogram,
-                &input,
-                UnifiedDiffBuilder::new(&input),
-            ),
-        }
-    }
 }
 
-impl WatchDiff<ConfigMap> for ConfigMap {
+impl ExtraInfo<ConfigMap> for ConfigMap {
     fn resource_name(&self) -> String {
         format!(
             "namespace {} configmap {}",
@@ -89,32 +101,15 @@ impl WatchDiff<ConfigMap> for ConfigMap {
             self.name_any()
         )
     }
-    fn yaml(&self) -> String {
-        serde_yaml::to_string(&self).unwrap()
-    }
     fn change_type(&self, previous: &ConfigMap) -> ChangeType {
         if self.data != previous.data {
             return ChangeType::Spec;
         }
         ChangeType::Metadata
     }
-    fn diff(&self, previous: &ConfigMap) -> ObjectDiff {
-        let src: String = previous.yaml();
-        let dst: String = self.yaml();
-        let input = InternedInput::new(src.as_str(), dst.as_str());
-        ObjectDiff {
-            change: self.change_type(previous),
-            resource_name: self.resource_name(),
-            diff: diff(
-                Algorithm::Histogram,
-                &input,
-                UnifiedDiffBuilder::new(&input),
-            ),
-        }
-    }
 }
 
-impl WatchDiff<Secret> for Secret {
+impl ExtraInfo<Secret> for Secret {
     fn resource_name(&self) -> String {
         format!(
             "namespace {} secret {}",
@@ -122,37 +117,17 @@ impl WatchDiff<Secret> for Secret {
             self.name_any()
         )
     }
-    fn yaml(&self) -> String {
-        serde_yaml::to_string(&self).unwrap()
-    }
     fn change_type(&self, previous: &Secret) -> ChangeType {
         if self.data != previous.data {
             return ChangeType::Spec;
         }
         ChangeType::Metadata
     }
-    fn diff(&self, previous: &Secret) -> ObjectDiff {
-        let src: String = previous.yaml();
-        let dst: String = self.yaml();
-        let input = InternedInput::new(src.as_str(), dst.as_str());
-        ObjectDiff {
-            change: self.change_type(previous),
-            resource_name: self.resource_name(),
-            diff: diff(
-                Algorithm::Histogram,
-                &input,
-                UnifiedDiffBuilder::new(&input),
-            ),
-        }
-    }
 }
 
-impl WatchDiff<Node> for Node {
+impl ExtraInfo<Node> for Node {
     fn resource_name(&self) -> String {
         format!("node {}", self.name_any())
-    }
-    fn yaml(&self) -> String {
-        serde_yaml::to_string(&self).unwrap()
     }
     fn change_type(&self, previous: &Node) -> ChangeType {
         if self.spec != previous.spec {
@@ -163,107 +138,27 @@ impl WatchDiff<Node> for Node {
         }
         ChangeType::Metadata
     }
-    fn diff(&self, previous: &Node) -> ObjectDiff {
-        let src: String = previous.yaml();
-        let dst: String = self.yaml();
-        let input = InternedInput::new(src.as_str(), dst.as_str());
-        ObjectDiff {
-            change: self.change_type(previous),
-            resource_name: self.resource_name(),
-            diff: diff(
-                Algorithm::Histogram,
-                &input,
-                UnifiedDiffBuilder::new(&input),
-            ),
-        }
-    }
 }
 
-pub async fn watch_pods(client: Client) -> impl Stream<Item = Result<ObjectDiff, anyhow::Error>> {
-    stream! {
-        let mut cache: HashMap<String, Pod> = HashMap::new();
-
-        let client_api: Api<Pod> = Api::all(client);
-        let (_, writer) = reflector::store::<Pod>();
-        let stream = watcher(client_api, watcher::Config::default().any_semantic())
-            .default_backoff()
-            .reflect(writer)
-            .applied_objects()
-            .predicate_filter(predicates::resource_version);
-        pin_mut!(stream);
-
-        while let Some(current) = stream.try_next().await? {
-            let mut diff : Option<ObjectDiff> = None;
-            cache.entry(current.resource_name()).and_modify(|previous| {
-                diff = Some(current.diff(previous));
-                *previous = current.clone();
-            }).or_insert(current);
-            if let Some(diff_value) = diff {
-                yield Ok(diff_value)
-            }
-        }
-    }
-}
-
-pub async fn watch_configmaps(
+pub async fn watch_resource<T>(
     client: Client,
-) -> impl Stream<Item = Result<ObjectDiff, anyhow::Error>> {
-    let mut cache: HashMap<String, ConfigMap> = HashMap::new();
+) -> impl Stream<Item = Result<ObjectDiff, anyhow::Error>>
+where
+    T: kube::Resource
+        + std::clone::Clone
+        + std::marker::Send
+        + std::fmt::Debug
+        + WatchDiff<T>
+        + ExtraInfo<T>
+        + for<'de> k8s_openapi::serde::Deserialize<'de>,
+    <T as kube::Resource>::DynamicType:
+        std::clone::Clone + std::default::Default + std::cmp::Eq + std::hash::Hash,
+{
     stream! {
-        let client_api: Api<ConfigMap> = Api::all(client);
-        let (_, writer) = reflector::store::<ConfigMap>();
-        let stream = watcher(client_api, watcher::Config::default().any_semantic())
-            .default_backoff()
-            .reflect(writer)
-            .applied_objects()
-            .predicate_filter(predicates::resource_version);
-        pin_mut!(stream);
+        let mut cache: HashMap<String, T> = HashMap::new();
 
-        while let Some(current) = stream.try_next().await? {
-            let mut diff : Option<ObjectDiff> = None;
-            cache.entry(current.resource_name()).and_modify(|previous| {
-                diff = Some(current.diff(previous));
-                *previous = current.clone();
-            }).or_insert(current);
-            if let Some(diff_value) = diff {
-                yield Ok(diff_value)
-            }
-        }
-    }
-}
-
-pub async fn watch_secrets(
-    client: Client,
-) -> impl Stream<Item = Result<ObjectDiff, anyhow::Error>> {
-    let mut cache: HashMap<String, Secret> = HashMap::new();
-    stream! {
-        let client_api: Api<Secret> = Api::all(client);
-        let (_, writer) = reflector::store::<Secret>();
-        let stream = watcher(client_api, watcher::Config::default().any_semantic())
-            .default_backoff()
-            .reflect(writer)
-            .applied_objects()
-            .predicate_filter(predicates::resource_version);
-        pin_mut!(stream);
-
-        while let Some(current) = stream.try_next().await? {
-            let mut diff : Option<ObjectDiff> = None;
-            cache.entry(current.resource_name()).and_modify(|previous| {
-                diff = Some(current.diff(previous));
-                *previous = current.clone();
-            }).or_insert(current);
-            if let Some(diff_value) = diff {
-                yield Ok(diff_value)
-            }
-        }
-    }
-}
-
-pub async fn watch_nodes(client: Client) -> impl Stream<Item = Result<ObjectDiff, anyhow::Error>> {
-    let mut cache: HashMap<String, Node> = HashMap::new();
-    stream! {
-        let client_api: Api<Node> = Api::all(client);
-        let (_, writer) = reflector::store::<Node>();
+        let client_api: Api<T> = Api::all(client);
+        let (_, writer) = reflector::store::<T>();
         let stream = watcher(client_api, watcher::Config::default().any_semantic())
             .default_backoff()
             .reflect(writer)
